1fe234fdc0e76ec6a8e68417f6b0d647c1ac5546
WilckerNeckel <wilckerwrsn@gmail.com>
2025-07-14 21:41:35 -0400

    feat: implements student gateway

--- src/student/core/gateways/StudentGateway.ts
+++ src/student/core/gateways/StudentGateway.ts
@@ -2,5 +2,5 @@ import { Student } from "../entities/Student";
 
 // Abstracts the persistence; the use case doesn't know the implementation details.
 export interface StudentGateway {
-    save(student: Student): Promise<void>;
+    save(student: Student): Promise<Student>;
 }
