2462e02090bcba77327c0cfa0d5d2e06266b679a
WilckerNeckel <wilckerwrsn@gmail.com>
2025-07-14 21:43:45 -0400

    feat: create base for student mongo db repository

--- src/student/infrastructure/StudentMongoRepository.ts
+++ src/student/infrastructure/StudentMongoRepository.ts
@@ -2,7 +2,7 @@ import { Student } from "../core/entities/Student";
 import { StudentGateway } from "../core/gateways/StudentGateway";
 
 export class StudentMongoRepository implements StudentGateway {
-    async save(student: Student): Promise<void> {
+    async save(student: Student): Promise<Student> {
         throw new Error("This method has not implemented yet");
     }
 }
