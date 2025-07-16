798de4180b8e41a3b7b473c5d1c81dc68f8ab606
WilckerNeckel <wilckerwrsn@gmail.com>
2025-07-15 22:15:50 -0400

    feat: finish student routes

--- src/student/presentation/student-routes.ts
+++ src/student/presentation/student-routes.ts
@@ -1,16 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { StudentMongoRepository } from "../infrastructure/StudentMongoRepository";
-import { CreateStudentInteractor } from "../use-cases/interactors/CreateStudentInteractor";
-import { CreateStudentController } from "./controllers/CreateStudentController";
-import { JsonCreateStudentPresenter } from "./presenters/JsonCreateStudentPresenter";
+import { makeCreateStudentController } from "./controllers/CreateStudentController";
 
 export async function studentRoutes(fastify: FastifyInstance) {
     fastify.post("/students", async (req, res) => {
-        const studentGateway = new StudentMongoRepository();
-        const presenter = new JsonCreateStudentPresenter(res);
-        const useCase = new CreateStudentInteractor(studentGateway, presenter);
-        const controller = new CreateStudentController(useCase);
-
-        controller.handle(req, res);
+        await makeCreateStudentController(req, res).handle(req, res);
     });
 }
