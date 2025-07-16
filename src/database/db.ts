2ca48b348cffe65cf47a2422428ebf8c500b36d4
WilckerNeckel <wilckerwrsn@gmail.com>
2025-07-15 22:34:07 -0400

    fix: adjust mongo client

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_URI! ?? "mongodb://localhost:27017";
+const uri = process.env.MONGO_URI! ?? "mongodb://root:example@localhost:27017?authSource=admin";
 
 const client = new MongoClient(uri);
 
