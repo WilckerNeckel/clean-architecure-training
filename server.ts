20d676794834324727be62264cf5c70dd679015d
WilckerNeckel <wilckerwrsn@gmail.com>
2025-07-15 22:16:04 -0400

    feat: registry the error handler

--- server.ts
+++ server.ts
@@ -3,25 +3,20 @@ import Fastify from "fastify";
 import { studentRoutes } from "./src/student/presentation/student-routes";
 import { ensureEnvIsConfigured } from "./scripts/ensureEnvIsConfigured";
 import dotenv from "dotenv";
+import { errorHandler } from "./src/shared/middlewares/error-middeware";
 
 const fastify = Fastify({
     logger: true,
 });
 
-fastify.register(studentRoutes);
+fastify.setErrorHandler(errorHandler);
 
-fastify.listen({ port: 3000 }, function (err, address) {
-    if (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-    // Server is now listening on ${address}
-});
+fastify.register(studentRoutes);
 
 const start = async () => {
     try {
-        dotenv.config();
-        ensureEnvIsConfigured();
+        // dotenv.config();
+        // ensureEnvIsConfigured();
         await fastify.listen({ port: 3000, host: "0.0.0.0" });
         console.log(`Server is running at http://localhost:3000`);
     } catch (err) {
