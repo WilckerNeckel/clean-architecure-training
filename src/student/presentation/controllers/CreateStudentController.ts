9d8392f3f1e2abbcab92c9ec0b3d75b621133190
WilckerNeckel <wilckerwrsn@gmail.com>
2025-07-15 22:11:00 -0400

    feat: finish create student controller implementation

--- src/student/presentation/controllers/CreateStudentController.ts
+++ src/student/presentation/controllers/CreateStudentController.ts
@@ -1,20 +1,25 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { StudentValidator } from "../../domain/ports/StudentValidator";
 import { CreateStudentInputBoundary } from "../../use-cases/input-boundary/CreateStudentInputBoundary";
 
 export class CreateStudentController {
     constructor(
-        private readonly createStudentUseCase: CreateStudentInputBoundary
+        private readonly createStudentUseCase: CreateStudentInputBoundary,
+        private readonly studentValidator: StudentValidator
     ) {}
 
-    async handle(req: any, res: any): Promise<void> {
-        const requestModel = {
-            registrationNumber: req.body.registrationNumber,
-            name: req.body.name,
-            age: req.body.age,
-            course: req.body.course,
-            admissionDate: new Date(req.body.admissionDate),
-        };
-
-        await this.createStudentUseCase.execute(requestModel);
+    async handle(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+        try {
+            const requestModel = this.studentValidator.validateInputCreation(
+                request.body
+            );
+            const result = await this.createStudentUseCase.execute(
+                requestModel
+            );
+            reply.send(result);
+        } catch (error) {
+            throw error;
+        }
     }
 }
 // 💬 Receives HTTP input, builds the request model, and passes it to the use case.
